33a7d56c80619d271ba15a23a28734ee27f91a33
kavehpzh <dantepzh@gmail.com>
2025-10-08 14:20:15 +0330

    fix orange highlight bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
     // --------------------------------------------
     void HighlightValidMoves()
     {
+        boardManager.ClearAllHighlights();
+        
         // Show small indicators instead of tinting tiles
         boardManager.ShowMoveIndicators(piece);
 
